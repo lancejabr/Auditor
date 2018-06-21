6711e40d65ae853a27cb7fd56270cb3c244c5ba2
Lance Jabr <lancejabr@gmail.com>
2018-06-21 13:29:04 -0700

    Initial Commit

---
+++ Auditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Auditor
+//
+//  Created by Lance Jabr on 6/21/18.
+//  Copyright © 2018 Lance Jabr. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
